51ab951b27a54ed5995c7e16d72a2868a1d5aacd
Jake P. <jakep316@gmail.com>
2026-02-03 22:23:15 -0500

    Fix h2h page module export

---
+++ src/app/h2h/page.tsx
@@ -0,0 +1,8 @@
+export default function H2HPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Head-to-Head</h1>
+      <p>H2H page placeholder (module check).</p>
+    </div>
+  );
+}
